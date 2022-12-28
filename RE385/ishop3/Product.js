bb7bfa6fa2fbfa695d792fde7ade83b8cdccf300
Siarhei Ruban <dnmneon4uk@gmail.com>
2022-12-28 12:49:34 +0300

    Initial commit

---
+++ RE385/ishop3/Product.js
@@ -0,0 +1,44 @@
+let Product = React.createClass({
+	displayName: 'Product',
+
+	propTypes: {
+		id: React.PropTypes.number,
+		productName: React.PropTypes.string,
+		price: React.PropTypes.number,
+		rest: React.PropTypes.number,
+		photoUrl: React.PropTypes.string,
+		isSelected: React.PropTypes.bool,
+		cbSelectedItem: React.PropTypes.func,
+		cbDeleteItem: React.PropTypes.func,
+	},
+
+	select: function (eo) {
+		this.props.cbSelectedItem(this.props.id);
+	},
+
+	delete: function (eo) {
+		eo.stopPropagation();
+		this.props.cbDeleteItem(this.props.id);
+	},
+
+	render: function () {
+		return React.DOM.tr({ onClick: this.select, className: (this.props.isSelected) ? 'selected' : null },
+			React.DOM.td({ className: 'productName' }, this.props.productName),
+			React.DOM.td({ className: 'price' }, this.props.price),
+			React.DOM.td({ className: 'stockBalance' }, this.props.rest),
+			React.DOM.td({ className: 'photoCell' },
+				React.DOM.img({
+					className: 'img',
+					src: this.props.photoUrl
+				})),
+			React.DOM.td({},
+				React.DOM.input({
+					className: 'deleteBtn',
+					type: 'button',
+					value: 'Delete',
+					onClick: this.delete
+				},),
+			)
+		)
+	}
+});
