1e1d647cb4b748f32e26d3e0f34cd162f9339661
Siarhei Ruban <dnmneon4uk@gmail.com>
2022-12-29 10:34:54 +0300

    update:
    Переделал компоненты на классовый синтаксис ES6.

--- RE385/ishop3/App.js
+++ RE385/ishop3/App.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Ishop from './components/Ishop';
 
