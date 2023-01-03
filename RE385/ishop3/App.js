e8074e433a9ac359357c2b2d06437ab683e232bf
Siarhei Ruban <dnmneon4uk@gmail.com>
2023-01-03 11:40:25 +0300

    fix:
    исправил синтаксис пропсов в компонентах

--- RE385/ishop3/App.js
+++ RE385/ishop3/App.js
@@ -5,7 +5,15 @@ import Ishop from './components/Ishop';
 
 const storeName = 'iFruitStore';
 
-import productsArr from './products.json';
+// const productsArr = require('./products.json');
+// import productsArr from './products.json';
+const productsArr = [
+	{ productName: 'Carrot', price: 10, photoUrl: 'img/carrot.jpg', rest: 5, id: 1, },
+	{ productName: 'Apple', price: 1, photoUrl: 'img/apple.jpg', rest: 99, id: 2, },
+	{ productName: 'Pear', price: 3, photoUrl: 'img/pear.jpg', rest: 22, id: 3, },
+	{ productName: 'Orange', price: 5, photoUrl: 'img/orange.jpg', rest: 15, id: 4, },
+	{ productName: 'Grape', price: 79, photoUrl: 'img/grape.jpg', rest: 52, id: 5, },
+];
 
 
 ReactDOM.render(
@@ -15,3 +23,12 @@ ReactDOM.render(
 	/>
 	, document.querySelector('#container')
 );
+
+
+// ReactDOM.render(
+// 	<Ishop
+// 		product={productsArr}
+// 		name={storeName}
+// 	/>
+// 	, document.querySelector('#container')
+// );
